8b55bae1e6bf8e2980f0e91fbaf55efa6a9de18c
victorien <deadvicmangame@gmail.com>
2022-11-22 14:28:29 +0100

    exo71

--- exercice1.js
+++ exercice1.js
@@ -1,9 +1,12 @@
-let monDiv = .createElement('div');
-
+let monDiv= document.createElement('div');
+monDiv.innerHTML= "Ce div a été ajouté en utilisant javascript";
 document.getElementById('viewport').appendChild(monDiv);
+monDiv.style.backgroundColor = "red";
+monDiv.style.height = '100px';
+monDiv.style.width = '100px';
 
-
-
+let sup = document.getElementById("aSupprimer");
+sup.remove("aSupprimer");
 
 
 
